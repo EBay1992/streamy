28d0085309668e83df93bb523c531b2d2da069fb
Ehsan <ehsanbayranvand@gmail.com>
2021-11-03 16:45:19 +0330

    built show page

--- src/Components/Streams/StreamList.jsx
+++ src/Components/Streams/StreamList.jsx
@@ -26,13 +26,17 @@ class StreamList extends Component {
     }
   };
 
-  renderStreams = () => {
+  renderList = () => {
     return this.props.streams.map((stream) => {
       return (
         <div className="item" key={stream.id}>
           {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
-          <div className="content">{stream.title}</div>
+          <div className="content">
+            <Link to={`/streams/${stream.id}`} className="header">
+              {stream.title}
+            </Link>
+          </div>
           <div className="description">{stream.description}</div>
         </div>
       );
@@ -43,18 +47,19 @@ class StreamList extends Component {
     if (this.props.isSignedIn) {
       return (
         <div style={{ textAlign: "right" }}>
-          <Link to="/streams/create" className="ui button primary">
+          <Link to="/streams/new" className="ui button primary">
             Create Stream
           </Link>
         </div>
       );
     }
   };
+
   render() {
     return (
       <div>
         <h2>Streams</h2>
-        <div className="ui celled list">{this.renderStreams()}</div>
+        <div className="ui celled list">{this.renderList()}</div>
         {this.renderCreate()}
       </div>
     );
